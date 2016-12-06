28851e886bed48bc6605cd06acf362ee607b90c3
yofir <ofyotb1@gmail.com>
2016-12-07 01:04:26 +0200

    xxxx

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -11,6 +11,7 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("aaaaaaaaaaa");
+            Console.WriteLine("aaaaaaaaaaa");
         }
     }
 }
